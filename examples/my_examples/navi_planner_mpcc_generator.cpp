ccb1a2fab1f32a0c658ac15e1314bb39f45d910a
이승준 <lsj317@hyundai.com>
2023-10-16 10:34:11 +0900

    initial commit for vw model

---
+++ examples/my_examples/navi_planner_mpcc_generator.cpp
@@ -0,0 +1,91 @@
+#include <acado_toolkit.hpp>
+
+int main()
+{
+    USING_NAMESPACE_ACADO
+
+    // State & input variables
+    DifferentialState x, y, phi, theta;
+    Control v, w, v_theta;
+
+    // Differential equation
+    DifferentialEquation f;
+    f << dot(x) == v * cos(phi);
+    f << dot(y) == v * sin(phi);
+    f << dot(phi) == w;
+    f << dot(theta) == v_theta;
+
+    // Online data
+    //// weight (7)
+    OnlineData w_e_c;
+    OnlineData w_e_l;
+    OnlineData w_theta;
+    OnlineData w_dv;
+    OnlineData w_dw;
+    OnlineData w_dv_theta;
+    OnlineData w_ts;
+
+    //// path (3)
+    OnlineData phi_d;
+    OnlineData x_d;
+    OnlineData y_d;
+
+    //// kinematic constraints (4)
+    OnlineData v_max;
+    OnlineData v_min;
+    OnlineData w_max;
+    OnlineData w_min;
+
+    //// previous inputs (3)
+    OnlineData v_prev;
+    OnlineData w_prev;
+    OnlineData v_theta_prev;
+
+    //// terminal states (3)
+    OnlineData phi_t;
+    OnlineData x_t;
+    OnlineData y_t;
+
+    // Problem formulation
+    OCP ocp(0.0, 3.0, 15);
+    ocp.setNOD(20);
+    ocp.setModel(f);
+
+    //// constraints
+    ocp.subjectTo(0.0 <= v - v_min);
+    ocp.subjectTo(v - v_max <= 0.0);
+    ocp.subjectTo(v_theta - v <= 0.0);
+    ocp.subjectTo(0.0 <= w - w_min);
+    ocp.subjectTo(w - w_max <= 0.0);
+
+    //// cost function
+    Expression cost_tracking = w_e_c * pow(sin(phi_d) * (x - x_d) - cos(phi_d) * (y - y_d), 2) +
+                               w_e_l * pow(-cos(phi_d) * (x - x_d) - sin(phi_d) * (y - y_d), 2);
+    Expression cost_evolution = w_theta * theta;
+    Expression cost_delta_inputs = w_dv * pow(v - v_prev, 2) + w_dw * pow(w - w_prev, 2) + w_dv_theta * pow(v_theta - v_theta_prev, 2);
+    Expression cost_terminal_state = w_ts * (pow(x_t - x, 2) + pow(y_t - y, 2) + pow(phi_t - phi, 2));
+
+    ocp.minimizeLagrangeTerm(cost_tracking - cost_evolution + cost_delta_inputs + cost_terminal_state);
+
+    // Generate and export MPC code
+    OCPexport mpc(ocp);
+    mpc.set(HESSIAN_APPROXIMATION, EXACT_HESSIAN);
+    mpc.set(DISCRETIZATION_TYPE, MULTIPLE_SHOOTING);
+    mpc.set(INTEGRATOR_TYPE, INT_RK4);
+    mpc.set(NUM_INTEGRATOR_STEPS, 10);
+    mpc.set(QP_SOLVER, QP_QPOASES);
+    mpc.set(HOTSTART_QP, NO);
+    mpc.set(GENERATE_TEST_FILE, YES);
+    mpc.set(GENERATE_MAKE_FILE, YES);
+    mpc.set(GENERATE_MATLAB_INTERFACE, NO);
+    mpc.set(SPARSE_QP_SOLUTION, FULL_CONDENSING_N2);
+    mpc.set(DYNAMIC_SENSITIVITY, SYMMETRIC);
+    mpc.set(CG_HARDCODE_CONSTRAINT_VALUES, NO);
+    mpc.set(CG_USE_VARIABLE_WEIGHTING_MATRIX, YES);
+
+    if (mpc.exportCode("navi_planner_mpcc") != SUCCESSFUL_RETURN)
+        exit(EXIT_FAILURE);
+    mpc.printDimensionsQP();
+
+    return EXIT_SUCCESS;
+}
